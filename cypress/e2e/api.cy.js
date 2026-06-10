18d66871956cfc9350f626c03c08e6b2d8e51623
Keydsq10 <kydsq19@gmail.com>
2026-06-10 17:36:03 -0300

    Actualizo casos API

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -14,12 +14,18 @@ describe('Casos de prueba de APIs', () => {
     })
 
     it('API | Obtener libro existente exitosamente | Key De Sousa', () => {
-        cy.getBookAPI(book.bookId, 200)
+        cy.getBookAPI(
+            book.bookId, 
+            200
+        )
     }) // Consulto por un libro existente utilizando el ID almacenado en el fixture
 
 
     it('API | Error al obtener libro inexistente | Key De Sousa', () => {
-        cy.getBookAPI(999999, 404)
+        cy.getBookAPI(
+            999999, 
+            404
+        )
     })  // Consulto un ID inexistente para validar la respuesta de error
 
     it('API | Compra exitosa con múltiples libros | Vanesa Gonzalez', () => {
@@ -39,16 +45,26 @@ describe('Casos de prueba de APIs', () => {
     })
     
     it('API | Iniciar sesión exitosamente | Ciro Brito', () => {
-        cy.postLoginAPI(user.name, user.password, 200)
+        cy.postLoginAPI(
+            user.name, 
+            user.password, 
+            200
+        )
     })
 
     it('API | Error al iniciar sesión con datos en blanco | Ciro Brito', () => {
-        cy.postLoginAPI(user.blankName, user.blankPassword, 401)
+        cy.postLoginAPI(
+            user.blankName, 
+            user.blankPassword, 
+            401
+        )
     })
 
-    it('API | Caso de Prueba | Nombre de alumno', () => {
+    it('Consultar detalles de facturación guardados con token válido API | Marlon Jaramillo', () => {
+        cy.getBillingDetailsAPI(user.userId, user.token, 200)
     })
 
-    it('API | Caso de Prueba | Nombre de alumno', () => {
+    it('Error al intentar consultar detalles de facturación sin token | Marlon Jaramillo', () => {
+        cy.getBillingDetailsAPI(user.userId, '', 200)
     })
 })
